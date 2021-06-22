4b94ee964e8b9298985dae4581635b62def92169
Sanjay Mamidipaka <sanjay.mamidipaka@gmail.com>
2021-06-22 11:19:00 -0400

    some changes

--- .history/keyfigures_20210428115704.py
+++
@@ -1,4 +0,0 @@
-plevelspath = '/Users/sanjaymamidipaka/Downloads/ZSAPIBP1C_PA_ATTRIBUTES_2021-04-16_15_02.csv'
-
-masterdatatypes = pd.read_csv(masterdatapath, delimiter=';')
-plevels = pd.read_csv(plevelspath, delimiter=';')
--- .history/keyfigures_20210428115715.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import csv
-import xlsxwriter
-
-plevelspath = '/Users/sanjaymamidipaka/Downloads/ZSAPIBP1C_PA_ATTRIBUTES_2021-04-16_15_02.csv'
-
-masterdatatypes = pd.read_csv(masterdatapath, delimiter=';')
-plevels = pd.read_csv(plevelspath, delimiter=';')
